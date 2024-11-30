d3c6a7d43ae4ad3c239b5c607848dac018d1fd22
chetan panara <chetanpanara88@gmail.com>
2024-12-01 00:06:22 +0530

    commit

--- src/Component/Youtubevideo.jsx
+++ src/Component/Youtubevideo.jsx
@@ -14,11 +14,11 @@ export default function Youtubevideo() {
       <div class="album py-5 bg-body-tertiary">
     <div class="container">
 
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 g-3">
               
         <div class="col">
           <div class="card shadow-sm">
-           <iframe width="420" height="315" src="https://www.youtube.com/embed/5vmyYPvW88k?si=sgsfTfvB4nk2jHai" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+           <iframe src="https://www.youtube.com/embed/5vmyYPvW88k?si=sgsfTfvB4nk2jHai" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             <div class="card-body">
               <p class="card-text">How to create Registration Form in HTML & CSS || How to make Registration Form Using HTML and CSS</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -33,7 +33,7 @@ export default function Youtubevideo() {
               
                  <div class="col">
           <div class="card shadow-sm">
-          <iframe width="420" height="315" src="https://www.youtube.com/embed/5H-JTNGuI1U?si=z4b2FYm2DoWy9iWr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe  src="https://www.youtube.com/embed/5H-JTNGuI1U?si=z4b2FYm2DoWy9iWr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             <div class="card-body">
               <p class="card-text">How to create Registration Form in HTML & CSS || How to make Registration Form Using HTML and CSS</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -48,7 +48,7 @@ export default function Youtubevideo() {
               
                  <div class="col">
           <div class="card shadow-sm">
-         <iframe  width="420" height="315" src="https://www.youtube.com/embed/75OcvRXa330?si=7oZvMhQY3NFox2yZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+         <iframe  src="https://www.youtube.com/embed/75OcvRXa330?si=7oZvMhQY3NFox2yZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             <div class="card-body">
               <p class="card-text">How to make Logo using HTML & CSS | Instagram logo design using css</p>
               <div class="d-flex justify-content-between align-items-center">
