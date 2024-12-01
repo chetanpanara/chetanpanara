5b8eac3d728ec16ccab88fd29c4164eaf4002b71
chetan panara <chetanpanara88@gmail.com>
2024-12-01 15:50:07 +0530

    commit

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -66,21 +66,26 @@ export default function Project() {
             
          <div class="col-lg-3 col-md-6  d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
               
-              <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
          <div className="d-flex">
-                        <h4>Bistore Grocery Website</h4>
+                        <h4>Social App For Community Donations Website </h4>
                 </div>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
                   <div class="modal-body">
                      <div class="member-img">
-                <img src="./img/project2.jpg" class="img-fluid" alt=""/>
+                <img src="./img/project5.jpg" class="img-fluid" alt=""/>
               </div>
             <div class="member-info">           
-                <span>Developed features including User registration, login, and account management with secure authentication, Dynamic Shopping cart functionality allowing users to add, view, and remove products,checkout process and Admin panel for managing products,categories,orders, and user accounts with statistical insights.Emphasized responsive design and robust back-end integration to provide a reliable and user-friendly online shopping experience.</span>
+                <span>Features:
+User Login & Registration: Implemented secure user authentication for seamless access.
+Admin Panel: Developed an admin interface for managing users, events, and donations.
+Event Creation: Enabled users to create and manage fundraising events easily.
+Secure Donations: Integrated Razorpay for seamless and secure online donations for events.
+</span>
               </div>
       </div>
       {/* <div class="modal-footer">
@@ -91,9 +96,9 @@ export default function Project() {
   </div>
 </div>
 
-            <div class="team-member" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <div class="team-member" data-bs-toggle="modal" data-bs-target="#exampleModal1">
               <div class="member-img">
-                <img src="./img/project2.jpg" class="img-fluid" alt=""/>
+                <img src="./img/project5.jpg" class="img-fluid" alt=""/>
                 <div class="social">
                   <a href=""><i class="bi bi-twitter-x"></i></a>
                   <a href=""><i class="bi bi-facebook"></i></a>
@@ -103,10 +108,10 @@ export default function Project() {
               </div>
               <div class="member-info">
                   <div className="d-flex">
-                    <h4>Bistore Grocery Website</h4>
+                    <h4>Social App For Community Donations Website </h4>
                      <a href=""><i class="bi bi-github mx-4" ></i></a>
                 </div>
-                <span>Emphasized responsive design and robust back-end integration to provide a reliable and user-friendly online shopping experience.</span>
+                <span>The Social App for community donations helps connect people, raise funds, support local causes, and empower positive change through collective action.</span>
               </div>
             </div>
             </div>
