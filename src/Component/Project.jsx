b3a9db55e601527e656f6a2250809d33a99e39d9
chetan panara <chetanpanara88@gmail.com>
2024-12-04 23:15:49 +0530

    commit

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -46,7 +46,7 @@ export default function Project() {
 
               <div class="team-member">
                 <div className="row">
-                  <div className="col-lg-3">
+                  <div className="col-lg-3 col-md-4">
                                  <div class="member-img" data-bs-toggle="modal" data-bs-target="#exampleModal">
                                 <a href="#" className='float-end m-2'><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-up-right-circle-fill" viewBox="0 0 16 16">
   <path d="M0 8a8 8 0 1 0 16 0A8 8 0 0 0 0 8m5.904 2.803a.5.5 0 1 1-.707-.707L9.293 6H6.525a.5.5 0 1 1 0-1H10.5a.5.5 0 0 1 .5.5v3.975a.5.5 0 0 1-1 0V6.707z"/>
@@ -55,14 +55,15 @@ export default function Project() {
              
               </div>
                   </div>
-                  <div className="col-lg-8">
+                  <div className="col-lg-8 col-md-8">
        <div class="member-info">
                   <div className="d-flex">
                     <h4>Bistore Grocery Website</h4>
                      <a href="https://github.com/chetanpanara/Bigstore-e-commerce-website"  className='mx-2'><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
   <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
 </svg></a>
-                </div>
+                    </div>
+                    <p className='text-primary mt-2'><b>Feature:</b></p>
                 <span>Developed features including User registration, login, and account management with secure authentication, Dynamic Shopping cart functionality allowing users to add, view, and remove products,checkout process and Admin panel for managing products,categories,orders, and user accounts with statistical insights.Emphasized responsive design and robust back-end integration to provide a reliable and user-friendly online shopping experience.</span>
               <a href="https://github.com/chetanpanara/Bigstore-e-commerce-website/archive/refs/heads/main.zip" className='download mt-3'>Download</a>
                 </div>
