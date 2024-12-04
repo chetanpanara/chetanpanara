ec1aaaf075211c9b955216299d91ee444073b056
chetan panara <chetanpanara88@gmail.com>
2024-12-04 19:25:01 +0530

    commit

--- src/Component/Template.jsx
+++ src/Component/Template.jsx
@@ -21,9 +21,23 @@ export default function Template() {
                         <img src="./img/template/t1.webp" alt="" className='img-fluid' />
               </div>
               <div className="col-lg-6 col-md-6">
-                <h3>iLanding - Bootstrap Landing Page Template</h3>
+                <h3 className='text-primary'>iLanding - Bootstrap Landing Page Template</h3>
                 <p>iLanding is a clean and professional Bootstrap landing page template designed to showcase your business,
                   app, or service. iLanding provides a visually appealing experience that effortlessly highlights ...</p>
+                <div className="button">
+                  <a href="#" className='btn btn-outline-primary'>Live View</a>
+                   <a href="#" className='btn btn-primary mx-3'>Download</a>
+                </div>
+                </div>
+            </div>
+            
+            <div className="row justify-content-center">
+              <div className="col-lg-4 col-md-6">
+                        <img src="./img/template/t2.webp" alt="" className='img-fluid' />
+              </div>
+              <div className="col-lg-6 col-md-6">
+                <h3 className='text-primary'>FlexStart - Bootstrap Startup Template</h3>
+                <p>FlexStart is a creative and modern Bootstrap website template. FlexStart is created for individuals and companies who want to build websites for software, startup, mobile app, digital agency, SaaS, internet services, fintech and related services and products websites.</p>
                 <div className="button">
                   <a href="#" className='btn btn-outline-primary'>Live View</a>
                    <a href="#" className='btn btn-primary mx-3'>Download</a>
