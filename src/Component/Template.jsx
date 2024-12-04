6d597f7a4c946219704401a570cbdde938eb8a31
chetan panara <chetanpanara88@gmail.com>
2024-12-04 19:35:04 +0530

    commit

--- src/Component/Template.jsx
+++ src/Component/Template.jsx
@@ -16,14 +16,13 @@ export default function Template() {
           <div className="col-lg-12">
 
             
-            <div className="row justify-content-center">
+            <div className="row justify-content-center mb-3">
               <div className="col-lg-4 col-md-6">
-                        <img src="./img/template/t1.webp" alt="" className='img-fluid' />
+                        <img src="./img/template/t3.png" alt="" className='img-fluid' />
               </div>
               <div className="col-lg-6 col-md-6">
-                <h3 className='text-primary'>iLanding - Bootstrap Landing Page Template</h3>
-                <p>iLanding is a clean and professional Bootstrap landing page template designed to showcase your business,
-                  app, or service. iLanding provides a visually appealing experience that effortlessly highlights ...</p>
+                <h3 className='text-primary'>Bigstore –Online Grocery Website</h3>
+                <p>Developed features including User registration, login, and account management with secure authentication, Dynamic Shopping cart functionality allowing users to add, view, and remove products,checkout process and Admin panel for managing products,categories,orders, and user accounts with statistical insights.Emphasized responsive design and robust back-end integration to provide a reliable and user-friendly online shopping experience.</p>
                 <div className="button">
                   <a href="#" className='btn btn-outline-primary'>Live View</a>
                    <a href="#" className='btn btn-primary mx-3'>Download</a>
@@ -33,11 +32,15 @@ export default function Template() {
             
             <div className="row justify-content-center">
               <div className="col-lg-4 col-md-6">
-                        <img src="./img/template/t2.webp" alt="" className='img-fluid' />
+                        <img src="./img/template/t4.png" alt="" className='img-fluid' />
               </div>
               <div className="col-lg-6 col-md-6">
-                <h3 className='text-primary'>FlexStart - Bootstrap Startup Template</h3>
-                <p>FlexStart is a creative and modern Bootstrap website template. FlexStart is created for individuals and companies who want to build websites for software, startup, mobile app, digital agency, SaaS, internet services, fintech and related services and products websites.</p>
+                <h3 className='text-primary'>Social App –For Community Donations Website </h3>
+                <p>User Login & Registration: Implemented secure user authentication for seamless access.
+Admin Panel: Developed an admin interface for managing users, events, and donations.
+Event Creation: Enabled users to create and manage fundraising events easily.
+Secure Donations: Integrated Razorpay for seamless and secure online donations for events.
+</p>
                 <div className="button">
                   <a href="#" className='btn btn-outline-primary'>Live View</a>
                    <a href="#" className='btn btn-primary mx-3'>Download</a>
