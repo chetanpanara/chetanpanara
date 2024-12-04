3936313efd70d7af538b8e645f87d993fd8ece7c
chetan panara <chetanpanara88@gmail.com>
2024-12-04 19:45:25 +0530

    commit

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
     
       <section id="hero" class="hero section mt-5">
         
-      <div class="container">
+      {/* <div class="container">
         <div class="row gy-4">
           <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
             <h1>Hello!! I'm Chetan Panara</h1>
@@ -19,6 +19,22 @@ export default function Hero() {
             </div>
           </div>
           <div class="col-lg-6 order-1 order-lg-2 hero-img">
+            <img src="./img/hero-img.png" class="img-fluid animated" alt=""/>
+          </div>
+        </div>
+      </div> */}
+        
+         <div class="container-fluid">
+        <div class="row gy-4 justify-content-center">
+          <div class="col-lg-5 offset-lg-1 col-md-7 order-2 order-lg-1 d-flex flex-column justify-content-center">
+            <h1>Hi!!, I'm Chetan Panara</h1>
+            <p>I'am Full stack developer to create robust,secure and scalable web applications using MERN stack.</p>
+            <div class="d-flex">
+              <Link to="/about" class="btn-get-started">Get Started</Link>
+
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-5 order-1 order-lg-2 hero-img">
             <img src="./img/hero-img.png" class="img-fluid animated" alt=""/>
           </div>
         </div>
