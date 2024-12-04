9c70377777912d3a57f33859cf0ed67cdcd07fbd
chetan panara <chetanpanara88@gmail.com>
2024-12-04 19:12:31 +0530

    commit

--- src/Component/Template.jsx
+++ src/Component/Template.jsx
@@ -11,19 +11,19 @@ export default function Template() {
         <p>Discover an exceptional collection of Bootstrap Templates & Themes crafted with finesse. Explore our range of free and premium templates, designed to elevate your web presence.</p>.
       </div>
 
-      <div className="container">
+      <div className="container-fluid">
         <div className="row">
           <div className="col-lg-12">
 
             
             <div className="row justify-content-center">
-              <div className="col-lg-6 col-md-6">
+              <div className="col-lg-4 col-md-6">
                         <img src="./img/template/t1.webp" alt="" className='img-fluid' />
               </div>
               <div className="col-lg-6 col-md-6">
                 <h3>iLanding - Bootstrap Landing Page Template</h3>
                 <p>iLanding is a clean and professional Bootstrap landing page template designed to showcase your business,
-                  app, or service. With its sleek design and intuitive layout, iLanding provides a visually appealing experience that effortlessly highlights ...</p>
+                  app, or service. iLanding provides a visually appealing experience that effortlessly highlights ...</p>
                 <div className="button">
                   <a href="#" className='btn btn-outline-primary'>Live View</a>
                    <a href="#" className='btn btn-primary mx-3'>Download</a>
