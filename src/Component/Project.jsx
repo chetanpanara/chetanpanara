5e17f29ab3ca8a88150422f8aaa4740e8f0ab0fb
chetan panara <chetanpanara88@gmail.com>
2024-12-04 22:27:24 +0530

    commit

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -12,14 +12,14 @@ export default function Project() {
         <p>Let's see I have completed some projects. you can get free source code in github link.</p>.
       </div>
 
-      <div class="container-fluid p-3">
+      <div class="container">
 
           <div class="row gy-4">
 
             
             
 
-            <div class="col-lg-4 col-md-6  d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
+            <div class="col-lg-3 col-md-6  d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
               
               <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
@@ -34,7 +34,15 @@ export default function Project() {
                      <div class="member-img">
                 <img src="./img/project2.jpg" class="img-fluid" alt=""/>
               </div>
-            <div class="member-info">           
+                      <div class="member-info">   
+                    <div className="language d-flex mb-3">
+                          <button type="button" class="btn rounded-pill btn-outline-primary btn-sm">PHP</button>  
+                          <button type="button" class="btn rounded-pill btn-outline-primary btn-sm mx-2">HTML</button>
+                          <button type="button" class="btn rounded-pill btn-outline-primary btn-sm ">CSS</button> 
+                               <button type="button" class="btn rounded-pill btn-outline-primary btn-sm mx-2">Javascript</button>  
+                          <button type="button" class="btn rounded-pill btn-outline-primary btn-sm ">Bootstrap</button>
+                             <button type="button" class="btn rounded-pill btn-outline-primary btn-sm mx-2">MYSQL</button>
+                        </div>           
                 <span>Developed features including User registration, login, and account management with secure authentication, Dynamic Shopping cart functionality allowing users to add, view, and remove products,checkout process and Admin panel for managing products,categories,orders, and user accounts with statistical insights.Emphasized responsive design and robust back-end integration to provide a reliable and user-friendly online shopping experience.</span>
               </div>
       </div>
@@ -47,7 +55,10 @@ export default function Project() {
 </div>
 
             <div class="team-member">
-              <div class="member-img" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <div class="member-img" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <a href="#" className='float-end m-2'><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-up-right-circle-fill" viewBox="0 0 16 16">
+  <path d="M0 8a8 8 0 1 0 16 0A8 8 0 0 0 0 8m5.904 2.803a.5.5 0 1 1-.707-.707L9.293 6H6.525a.5.5 0 1 1 0-1H10.5a.5.5 0 0 1 .5.5v3.975a.5.5 0 0 1-1 0V6.707z"/>
+</svg></a>
                 <img src="./img/project2.jpg" class="img-fluid" alt=""/>
                 {/* <div class="social">
                   <a href="https://www.instagram.com/_chetan_0207"><i class="bi bi-instagram"></i></a>
@@ -57,7 +68,9 @@ export default function Project() {
               <div class="member-info">
                   <div className="d-flex">
                     <h4>Bistore Grocery Website</h4>
-                     <a href="https://github.com/chetanpanara/Bigstore-e-commerce-website"><i class="bi bi-github mx-4" ></i></a>
+                     <a href="https://github.com/chetanpanara/Bigstore-e-commerce-website"  className='mx-2'><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+</svg></a>
                 </div>
                 <span>Developed features including User registration, login with secure authentication, Dynamic Shopping cart functionality allowing users to add, view, and remove products,checkout process and Admin panel for managing products,categories,orders, and user accounts with statistical insights.</span>
               <a href="https://github.com/chetanpanara/Bigstore-e-commerce-website/archive/refs/heads/main.zip" className='download mt-3 w-100'>Download</a>
@@ -65,7 +78,7 @@ export default function Project() {
             </div>
             </div>
             
-         <div class="col-lg-4 col-md-6  d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
+         <div class="col-lg-3 col-md-6  d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
               
               <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
@@ -80,14 +93,21 @@ export default function Project() {
                      <div class="member-img">
                 <img src="./img/project5.jpg" class="img-fluid" alt=""/>
               </div>
-            <div class="member-info">           
+                      <div class="member-info">    
+               <div className="language d-flex mb-3">
+                          <button type="button" class="btn rounded-pill btn-outline-primary btn-sm">PHP</button>  
+                          <button type="button" class="btn rounded-pill btn-outline-primary btn-sm mx-2">HTML</button>
+                          <button type="button" class="btn rounded-pill btn-outline-primary btn-sm ">CSS</button> 
+                               <button type="button" class="btn rounded-pill btn-outline-primary btn-sm mx-2">Javascript</button>  
+                          <button type="button" class="btn rounded-pill btn-outline-primary btn-sm ">Bootstrap</button>
+                             <button type="button" class="btn rounded-pill btn-outline-primary btn-sm mx-2">MYSQL</button>
+                        </div>          
                 <span>Features:
 User Login & Registration: Implemented secure user authentication for seamless access.
 Admin Panel: Developed an admin interface for managing users, events, and donations.
 Event Creation: Enabled users to create and manage fundraising events easily.
 Secure Donations: Integrated Razorpay for seamless and secure online donations for events.
                         </span>
-                                      
               </div>
       </div>
       {/* <div class="modal-footer">
@@ -99,7 +119,10 @@ Secure Donations: Integrated Razorpay for seamless and secure online donations f
 </div>
 
             <div class="team-member" >
-              <div class="member-img" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+                <div class="member-img" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+                  <a href="#" className='float-end m-2'><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-up-right-circle-fill" viewBox="0 0 16 16">
+  <path d="M0 8a8 8 0 1 0 16 0A8 8 0 0 0 0 8m5.904 2.803a.5.5 0 1 1-.707-.707L9.293 6H6.525a.5.5 0 1 1 0-1H10.5a.5.5 0 0 1 .5.5v3.975a.5.5 0 0 1-1 0V6.707z"/>
+</svg></a>
                 <img src="./img/project5.jpg" class="img-fluid" alt=""/>
                 {/* <div class="social">
                   <a href="https://www.instagram.com/_chetan_0207"><i class="bi bi-instagram"></i></a>
@@ -109,105 +132,9 @@ Secure Donations: Integrated Razorpay for seamless and secure online donations f
               <div class="member-info">
                   <div className="d-flex">
                     <h4>Social App For Community Donations Website </h4>
-                    <a href="https://github.com/chetanpanara/Social-app"><i class="bi bi-github mx-4"></i></a>
-                </div>
-                <span>The Social App for community donations helps connect people, raise funds, support local causes, and empower positive change through collective action.</span>
-                <a href="https://github.com/chetanpanara/Social-app/archive/refs/heads/main.zip" className='download mt-3 w-100'>Download</a>
-                </div>
-            </div>
-            </div>
-
-               <div class="col-lg-4 col-md-6  d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
-              
-              <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-         <div className="d-flex">
-                        <h4>Bistore Grocery Website</h4>
-                </div>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-                  <div class="modal-body">
-                     <div class="member-img">
-                <img src="./img/project2.jpg" class="img-fluid" alt=""/>
-              </div>
-            <div class="member-info">           
-                <span>Developed features including User registration, login, and account management with secure authentication, Dynamic Shopping cart functionality allowing users to add, view, and remove products,checkout process and Admin panel for managing products,categories,orders, and user accounts with statistical insights.Emphasized responsive design and robust back-end integration to provide a reliable and user-friendly online shopping experience.</span>
-              </div>
-      </div>
-      {/* <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div> */}
-    </div>
-  </div>
-</div>
-
-            <div class="team-member">
-              <div class="member-img" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                <img src="./img/project2.jpg" class="img-fluid" alt=""/>
-                {/* <div class="social">
-                  <a href="https://www.instagram.com/_chetan_0207"><i class="bi bi-instagram"></i></a>
-                  <a href="https://github.com/chetanpanara"><i class="bi bi-github"></i></a>
-                </div> */}
-              </div>
-              <div class="member-info">
-                  <div className="d-flex">
-                    <h4>Bistore Grocery Website</h4>
-                     <a href="https://github.com/chetanpanara/Bigstore-e-commerce-website"><i class="bi bi-github mx-4" ></i></a>
-                </div>
-                <span>Developed features including User registration, login with secure authentication, Dynamic Shopping cart functionality allowing users to add, view, and remove products,checkout process and Admin panel for managing products,categories,orders, and user accounts with statistical insights.</span>
-              <a href="https://github.com/chetanpanara/Bigstore-e-commerce-website/archive/refs/heads/main.zip" className='download mt-3 w-100'>Download</a>
-                </div>
-            </div>
-            </div>
-            
-         <div class="col-lg-4 col-md-6  d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
-              
-              <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-         <div className="d-flex">
-                        <h4>Social App For Community Donations Website </h4>
-                </div>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-                  <div class="modal-body">
-                     <div class="member-img">
-                <img src="./img/project5.jpg" class="img-fluid" alt=""/>
-              </div>
-            <div class="member-info">           
-                <span>Features:
-User Login & Registration: Implemented secure user authentication for seamless access.
-Admin Panel: Developed an admin interface for managing users, events, and donations.
-Event Creation: Enabled users to create and manage fundraising events easily.
-Secure Donations: Integrated Razorpay for seamless and secure online donations for events.
-                        </span>
-                                      
-              </div>
-      </div>
-      {/* <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div> */}
-    </div>
-  </div>
-</div>
-
-            <div class="team-member" >
-              <div class="member-img" data-bs-toggle="modal" data-bs-target="#exampleModal1">
-                <img src="./img/project5.jpg" class="img-fluid" alt=""/>
-                {/* <div class="social">
-                  <a href="https://www.instagram.com/_chetan_0207"><i class="bi bi-instagram"></i></a>
-                  <a href="https://github.com/chetanpanara"><i class="bi bi-github"></i></a>
-                </div> */}
-              </div>
-              <div class="member-info">
-                  <div className="d-flex">
-                    <h4>Social App For Community Donations Website </h4>
-                    <a href="https://github.com/chetanpanara/Social-app"><i class="bi bi-github mx-4"></i></a>
+                    <a href="https://github.com/chetanpanara/Social-app" className='mx-2'><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+</svg></a>
                 </div>
                 <span>The Social App for community donations helps connect people, raise funds, support local causes, and empower positive change through collective action.</span>
                 <a href="https://github.com/chetanpanara/Social-app/archive/refs/heads/main.zip" className='download mt-3 w-100'>Download</a>
