f0555b56a92c842aad69e79cf6db44be018b24ac
chetan panara <chetanpanara88@gmail.com>
2024-12-05 14:51:02 +0530

    commit

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -27,8 +27,55 @@ export default function Project() {
         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
                   <div className="modal-body">
-                     <div className="member-img">
-                <img src="./img/project2.jpg" className="img-fluid" alt=""/>
+                     <div className="member-img mb-3">
+                                    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./img/project/b1.png" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="./img/project/b2.png" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="./img/project/b3.png" class="d-block w-100" alt="..."/>
+                          </div>
+                          <div class="carousel-item">
+      <img src="./img/project/b4.png" class="d-block w-100" alt="..."/>
+                          </div>
+                          <div class="carousel-item">
+      <img src="./img/project/b5.png" class="d-block w-100" alt="..."/>
+                          </div>
+                          <div class="carousel-item">
+      <img src="./img/project/b6.png" class="d-block w-100" alt="..."/>
+                          </div>
+                           <div class="carousel-item">
+      <img src="./img/project/b7.png" class="d-block w-100" alt="..."/>
+                          </div>
+                           <div class="carousel-item">
+      <img src="./img/project/b8.png" class="d-block w-100" alt="..."/>
+                          </div>
+                           <div class="carousel-item">
+      <img src="./img/project/b9.png" class="d-block w-100" alt="..."/>
+                          </div>
+                           <div class="carousel-item">
+      <img src="./img/project/b10.png" class="d-block w-100" alt="..."/>
+                          </div>
+                           <div class="carousel-item">
+      <img src="./img/project/b11.png" class="d-block w-100" alt="..."/>
+                          </div>
+                              <div class="carousel-item">
+      <img src="./img/project/b12.png" class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
               </div>
                       <div className="member-info">   
                         <div className="language d-flex mb-3">
