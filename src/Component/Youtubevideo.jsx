a2fbcddb052b07df9c455359b3eee9e8c9908152
chetan panara <chetanpanara88@gmail.com>
2024-12-07 22:36:05 +0530

    commit

--- src/Component/Youtubevideo.jsx
+++ src/Component/Youtubevideo.jsx
@@ -6,20 +6,21 @@ export default function Youtubevideo() {
   return (
     <>
       
-    
-          <div className="container hero" id="hero">
+  
+
+           <div className="container" id="hero">
         <div className="row justify-content-center gy-4">
-          <div className="col-lg-7   order-2 order-lg-1 d-flex flex-column justify-content-center">
-            <h1 >Welcome in ckp Infotech</h1>
-            <p >Hey, I'm Developing unique projects and trying to make
+          <div className="col-lg-6   order-2 order-lg-1 d-flex flex-column justify-content-center">
+            <h1 className='heroh1'>Welcome in ckp Infotech</h1>
+            <p className='herop'>Hey, I'm Developing unique projects and trying to make
 web development learning easy for freshy users in this
 field. Fast track your development career with me. Hope
 you'll love my creativițy.</p>
             <div className="d-flex">
-              <Link to="https://www.youtube.com/@ckp_infotech" className="youtubebtn">Subscribe channel</Link>
+            <Link to="https://www.youtube.com/@ckp_infotech" className="youtubebtn">Subscribe channel</Link>
             </div>
           </div>
-          <div className="col-lg-5 order-1 order-lg-2 hero-img">
+          <div className="col-lg-6 order-1 order-lg-2 hero-img">
             <img src="./img/youtubehero.png" className="img-fluid animated" alt="not found"/>
           </div>
         </div>
