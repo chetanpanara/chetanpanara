bdc74b3e12b5962d537761ccb3539508aa89923c
chetan panara <chetanpanara88@gmail.com>
2024-12-08 17:33:36 +0530

    commit

--- src/Component/Youtubevideo.jsx
+++ src/Component/Youtubevideo.jsx
@@ -26,8 +26,8 @@ export default function Youtubevideo() {
           <iframe  src="https://www.youtube.com/embed/vZy2W4IIg2c?si=pUlcpRnekS-7ko_e" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
         <div className="youtube-video-card-content">
-          <h2 className="youtube-video-card-title">Video Title</h2>
-          <p className="youtube-video-card-description">Video description goes here. Video description goes here.</p>
+          <h2 className="youtube-video-card-title">How to send mail from contact form using ReactJS</h2>
+          <p className="youtube-video-card-description">How to send mail from contact form using ReactJS</p>
         </div>
       </div>
       <div className="youtube-video-card">
@@ -35,7 +35,7 @@ export default function Youtubevideo() {
         <iframe  src="https://www.youtube.com/embed/75OcvRXa330?si=imfKgoBTARzlAwyx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
         <div className="youtube-video-card-content">
-          <h2 className="youtube-video-card-title">Video Title</h2>
+          <h2 className="youtube-video-card-title">How to make Logo using HTML & CSS | Instagram logo design using css</h2>
           <p className="youtube-video-card-description">Video description goes here. Video description goes here.</p>
         </div>
       </div>
@@ -44,7 +44,7 @@ export default function Youtubevideo() {
         <iframe  src="https://www.youtube.com/embed/5H-JTNGuI1U?si=25QIWuEAQ8NITL_D" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
         <div className="youtube-video-card-content">
-          <h2 className="youtube-video-card-title">Video Title</h2>
+          <h2 className="youtube-video-card-title">How to create Registration Form in HTML & CSS || How to make Registration Form Using HTML and CSS</h2>
           <p className="youtube-video-card-description">Video description goes here. Video description goes here.</p>
         </div>
       </div>
