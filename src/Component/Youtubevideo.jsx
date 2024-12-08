15b02b56f95a48c68748555ba6be5d8597108977
chetan panara <chetanpanara88@gmail.com>
2024-12-08 21:05:14 +0530

    commit

--- src/Component/Youtubevideo.jsx
+++ src/Component/Youtubevideo.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import YoutubeNumber from './YoutubeNumber'
 import YoutubeHero from './YoutubeHero'
 
 export default function Youtubevideo() {
+
+  const [videos, setvideos] = useState([]);
+
+  
+  useEffect(() => { 
+    axios.get('http://localhost:8055/data')
+      .then(result => { 
+        setvideos(result.data)
+      })
+    .catch(err=>console.log(err))
+  },[])
+
   return (
     <>
       
@@ -20,34 +32,23 @@ export default function Youtubevideo() {
         </div>
         
 
-       <div className="youtube-video-card-container">
-      <div className="youtube-video-card">
+        <div className="youtube-video-card-container">
+          { 
+            videos.map((index) => (
+                                         <div className="youtube-video-card">
         <div className="youtube-video-card-video">
-          <iframe  src="https://www.youtube.com/embed/vZy2W4IIg2c?si=pUlcpRnekS-7ko_e" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;fullscreen" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                  <iframe src={index.link} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;fullscreen" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
         <div className="youtube-video-card-content">
-          <h2 className="youtube-video-card-title">How to send mail from contact form using ReactJS</h2>
-          <p className="youtube-video-card-description">How to send mail from contact form using ReactJS</p>
+                  <h2 className="youtube-video-card-title">{index.title}</h2>
+                  <p className="youtube-video-card-description">{index.description}</p>
         </div>
-      </div>
-      <div className="youtube-video-card">
-        <div className="youtube-video-card-video">
-        <iframe  src="https://www.youtube.com/embed/75OcvRXa330?si=imfKgoBTARzlAwyx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;fullscreen" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        </div>
-        <div className="youtube-video-card-content">
-          <h2 className="youtube-video-card-title">How to make Logo using HTML & CSS | Instagram logo design using css</h2>
-          <p className="youtube-video-card-description">Video description goes here. Video description goes here.</p>
-        </div>
-      </div>
-      <div className="youtube-video-card">
-        <div className="youtube-video-card-video">
-        <iframe  src="https://www.youtube.com/embed/5H-JTNGuI1U?si=25QIWuEAQ8NITL_D" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;fullscreen" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        </div>
-        <div className="youtube-video-card-content">
-          <h2 className="youtube-video-card-title">How to create Registration Form in HTML & CSS || How to make Registration Form Using HTML and CSS</h2>
-          <p className="youtube-video-card-description">Video description goes here. Video description goes here.</p>
-        </div>
-      </div>
+          </div>
+            ))
+          }
+      
+
+
     </div>
         
     </section>
