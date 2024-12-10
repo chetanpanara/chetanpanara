a29d95f086414ffdb98f76ee16158e03d19ad335
chetan panara <chetanpanara88@gmail.com>
2024-12-10 20:47:33 +0530

    commit

--- src/Component/Loadingspinner.jsx
+++ src/Component/Loadingspinner.jsx
@@ -4,7 +4,7 @@ export default function Loadingspinner() {
   return (
     <>
     
- <div className="youtube-video-card placeholder-glow">
+ <div className="youtube-video-card placeholder-glow" style={{borderStyle:"none"}}>
       <div className="youtube-video-card-video placeholder  loading"></div>
       <div className="youtube-video-card-content">
         <h2 className="youtube-video-card-title placeholder  loading"></h2>
